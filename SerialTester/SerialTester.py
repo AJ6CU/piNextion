194efb443e6494eab3dc553fca3a8e08b6d66b48
AJ6CU <70183884+AJ6CU@users.noreply.github.com>
2025-07-28 11:31:04 -0700

    initial sucess

--- SerialTester/SerialTester.py
+++ SerialTester/SerialTester.py
@@ -2,13 +2,11 @@
 import serial
 
 print(" starting serial test\n")
+ser = serial.Serial("/dev/ttyS0", 9600, timeout=1)
 
-if __name__ == '__main__':
-    ser = serial.Serial("/dev/ttyS0", 9600, timeout=1)
-
-    data_to_send = bytes([0x59, 0x58, 0x68, 0x06, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x73])
-    ser.write(data_to_send)
-    print("Sent: ")
-    print( data_to_send.hex())
-    print("\n")
-    ser.close()
+data_to_send = bytes([0x59, 0x58, 0x68, 0x06, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x73])
+ser.write(data_to_send)
+print("Sent: ")
+print( data_to_send.hex())
+print("\n")
+# ser.close()

454ff6f13cfbef8df8ccf6ea70b95dc4588ab0b8
AJ6CU <70183884+AJ6CU@users.noreply.github.com>
2025-07-29 13:35:01 -0700

    serialreader and sample files

--- SerialReader/serialReader.py
+++ SerialReader/serialReader.py
@@ -5,14 +5,45 @@ print(" starting serial test\n")
 ser = serial.Serial("/dev/ttyS0", 9600, timeout=1)
 
 try:
+    ffCount = 0
+    buffer = []
+    lineNum = 1
+    
     while True:
         # Read a line from the serial port (until a newline character is received)
         # Decode the bytes to a string (e.g., 'utf-8') and remove leading/trailing whitespace
-        data = ser.readline().decode('utf-8').strip()
+ 
+        in_byte= ser.read(1)
+        
+        if in_byte:
+            if (len(buffer) == 0):
+                print("line ", lineNum)
+            
+            buffer.append (in_byte)
 
-        if data:  # Check if data was received
-            print(f"Received: {data.hex()}")
+            if in_byte.hex() == 'ff':
+                ffCount += 1
+                if ffCount == 3:
 
+                    decoded_buffer = [item.decode(errors='replace') for item in buffer]
+                    for item in decoded_buffer:
+                        print(f"{item:<{4}}", end="")
+                    print("")
+                    
+                    decoded_buffer = [item.hex() for item in buffer]
+                    for item in decoded_buffer:
+                        print(f"{item:<{4}}", end="")
+                    print("")
+                    
+                    decoded_buffer = [ord(item) for item in buffer]
+                    for item in decoded_buffer:
+                        print(f"{item:<{4}}", end="")
+                    print("")
+                    print("\n")
+                    ffCount = 0
+                    lineNum += 1
+                    buffer = buffer[:0]
+ 
         time.sleep(0.1)  # Small delay to prevent busy-waiting
 
 except serial.SerialException as e:

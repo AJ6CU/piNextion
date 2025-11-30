f0fb773437aad124ccf1054916fc3f967ac8faf3
Mark Hatch <58341254+mark2382@users.noreply.github.com>
2025-11-30 11:51:21 -0800

    progress on window sizing

--- piCEC_UX/pygubu/piCEC_UX.py
+++ piCEC_UX/pygubu/piCEC_UX.py
@@ -25,7 +25,9 @@ myRadio = None
 #
 #   once a valid port is found, then we can start the main window.
 #
-def startMainWindow(comPortName, comPort):
+def startMainWindow(comPortName, comPortID):
+
+    comPort.place_forget()
 
     mainWindow.place(x=0, y=0)                          # place the mainWindow on the screen
     # mainWindow.pack()
@@ -40,10 +42,12 @@ def startMainWindow(comPortName, comPort):
 
     root.geometry(geo)
 
+    comPort.place(relx=0.80, rely=1, anchor="s")
+
     gv.config.setComPort(comPortName)  # update the config file if necessary because of comport selection
     print(" radio:", comPortName)
     # comPort.setComPort(comPortName)
-    myRadio = piRadio(comPortName, comPort, mainWindow)  # Initialize the Radio object with selected port
+    myRadio = piRadio(comPortName, comPortID, mainWindow)  # Initialize the Radio object with selected port
 
 
 
@@ -68,6 +72,7 @@ root = tk.Tk()
 # root.minsize(1020, 800)
 # root.minsize(775, 800)
 # root.geometry("1020x800")
+root.geometry("300x225")
 root.title("PiCEC - A Nextion Emulator for CEC Software")
 
 
@@ -109,13 +114,13 @@ comPort = comportManager(root,startMainWindow)
 # comPortWindow_height = comPort.winfo_height()
 # print("comPortWindow_height:",comPort.winfo_height())
 #
-comPort.place(relx=0.80, rely=1, anchor="s")
+# comPort.place(relx=0.80, rely=1, anchor="s")
 # x_pos = mainWindow_x_pos + mainWindow_width -10 - comPortWindow_width
 # y_pos = mainWindow_y_pos + mainWindow_height -10 - comPortWindow_height
 #
 # print(x_pos,y_pos)
 
-# comPort.place(x=x_pos, y=y_pos)
+comPort.place(x=0,y=0)
 
 if not comPort.getComPort():
     root.after(500, comPort.retry() )           # If we failed to get a comport the easy way, try again
